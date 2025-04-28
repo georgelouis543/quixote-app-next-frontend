31e8b229907ff5d4ea9bc4e831b9661293f547b8
georgelouis <george.louis@meltwater.com>
2025-04-29 00:47:05 +0530

    added error boundary and not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen text-center px-2">
+      
+      <h2 className="text-3xl font-bold mb-4">Page Not Found</h2>
+      
+      <Link href="/home" className="text-red-500 underline">
+        Return Home
+      </Link>
+      
+    </div>
+  );
+}
